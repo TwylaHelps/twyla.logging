9077ab88b0fd28cb6d4e3ffd900760e7efb0e59f
Ulas Turkmen <ulas.turkmen@twylahelps.com>
2017-09-22 21:47:42 +0200

    Added test for formatting log record

--- twyla/logging/tests/test_handlers.py
+++ twyla/logging/tests/test_handlers.py
@@ -1,5 +1,7 @@
 import unittest
 from unittest import mock
+import json
+
 from logging import LogRecord
 
 from twyla.logging import handlers
@@ -14,6 +16,21 @@ class LogglyHTTPSHandlerTestCase(unittest.TestCase):
     @mock.patch('twyla.logging.handlers.session')
     def test_handle_record(self, mock_session):
         handler = handlers.LogglyHTTPSHandler('abcd-123', 'python')
-        record = LogRecord('name', 'level', 'pathname', 'lineno', 'msg', [], None)
+        record = LogRecord('twyla.logging', 20, '/path/to/logging.py',
+                           11, 'The text message', args=[],
+                           exc_info=None, func='dostuff')
         handler.emit(record)
         assert mock_session.post.call_count == 1
+        _, (url,), kwargs = mock_session.post.mock_calls[0]
+        assert url == 'https://logs-01.loggly.com/inputs/abcd-123/tag/python'
+        data = json.loads(kwargs['data'])
+        assert data.pop('ascTime') == record.asctime
+        assert data.pop('time') == record.msecs
+        assert data.pop('logRecordCreationTime') == record.created
+        assert data == {'loggerName': 'twyla.logging',
+                        'fileName': 'logging.py',
+                        'functionName': 'dostuff',
+                        'levelNo': 20,
+                        'lineNo': 11,
+                        'levelName': 'INFO',
+                        'message': 'The text message'}
